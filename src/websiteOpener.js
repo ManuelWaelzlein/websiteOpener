52f8c652cf40937d5ec95955d278b6610226480b
ManuelWaelzlein <gockel98@freenet.de>
2018-06-14 22:11:35 +0200

    Git Setup

---
+++ src/websiteOpener.js
@@ -0,0 +1,31 @@
+var link = document.getElementById("linkField").value;
+const time = document.getElementById("timeField").value;
+const error = document.getElementById("error");
+
+function main() {
+    validate();
+    wait();
+    formOut();
+    textIn();
+    openLink();
+}
+
+function formOut() {
+
+}
+
+function textIn() {
+
+}
+
+function validate() {
+
+}
+
+function wait() {
+
+}
+
+function openLink() {
+    window.open();
+}
