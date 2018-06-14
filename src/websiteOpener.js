f4913d768cb5fae01586d4054d2c729be4fe232d
ManuelWaelzlein <gockel98@freenet.de>
2018-06-15 00:17:06 +0200

    Added and animated successText

--- src/websiteOpener.js
+++ src/websiteOpener.js
@@ -1,21 +1,36 @@
-var link = document.getElementById("linkField").value;
-const time = document.getElementById("timeField").value;
-const error = document.getElementById("error");
+var form;
+var successText;
+var time;
+var link;
+var error;
+
+
+window.onload = function openWebsite() {
+    form = document.getElementById("form");
+    successText = document.getElementById("successText");
+    time = document.getElementById("timeField");
+    link = document.getElementById("linkField");
+    error = document.getElementById("error");
+};
 
 function main() {
-    validate();
-    wait();
     formOut();
     textIn();
-    openLink();
 }
 
 function formOut() {
-
+    form.style.animationTimingFunction = "ease-out";
+    form.style.animationName = "Out";
+    form.style.left = "110%";
+    form.style.right = "-40%";
 }
 
-function textIn() {
-
+function textIn() { // TODO find solution to delay animation
+    successText.style.animationTimingFunction = "ease-in";
+    successText.style.animationName = "In";
+    successText.innerHTML = "Timer is set for 13:37";
+    successText.style.left = "35%";
+    successText.style.right = "35%";
 }
 
 function validate() {
